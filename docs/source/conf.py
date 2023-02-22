6b3200c67a852dbd4a718592118baf8047cb438b
Tyler <montro.l.nakuta@gmail.com>
2023-02-22 16:13:49 -0600

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -3,8 +3,8 @@
 # -- Project information
 
 project = "Molt's Test Docs"
-copyright = '2021, Graziella'
-author = 'Graziella'
+copyright = '2023, Molt'
+author = 'Molt'
 
 release = '0.1'
 version = '0.1.0'
